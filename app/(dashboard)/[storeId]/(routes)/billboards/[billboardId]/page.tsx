9da6c85a5c4e6adc668cb4719a0326d7cb825a27
cwarim-A <akoladequam3@gmail.com>
2025-01-09 11:05:15 +0100

    testing the BillbooardPage

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,27 +1,58 @@
-import prismadb from "@/lib/prismadb"
-import { BillboardForm } from "./components/billboard-form"
+// import prismadb from "@/lib/prismadb"
+// import { BillboardForm } from "./components/billboard-form"
 
 
-interface BillboardPageProps  {
+// interface BillboardPageProps  {
+//   params: { billboardId: string };
+// };
+
+
+// const BillboardPage = async ({ params }: BillboardPageProps) => {
+
+//     const Billboard = await prismadb.billboard.findUnique({
+//         where:{
+//             id:params.billboardId
+//         }
+//     })
+
+//   return (
+//     <div className="flex-col">
+//         <div className="flex-1 space-y-4 p-8 pt-6">
+//                 <BillboardForm initialData={Billboard}/>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default BillboardPage
+
+
+import prismadb from "@/lib/prismadb";
+import { BillboardForm } from "./components/billboard-form";
+
+interface BillboardPageProps {
   params: { billboardId: string };
-};
-
+}
 
 const BillboardPage = async ({ params }: BillboardPageProps) => {
+  const billboard = await prismadb.billboard.findUnique({
+    where: {
+      id: params.billboardId,
+    },
+  });
 
-    const Billboard = await prismadb.billboard.findUnique({
-        where:{
-            id:params.billboardId
-        }
-    })
+  if (!billboard) {
+    return <div>Billboard not found</div>;
+  }
 
   return (
     <div className="flex-col">
-        <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardForm initialData={Billboard}/>
-        </div>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardForm initialData={billboard} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BillboardPage
+export default BillboardPage;
+
